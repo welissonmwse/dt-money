f8f245e3a45561ffbf00f9f99fdcd0e0ddfb167e
Welisson Silveira <welissonmwse@gmail.com>
2022-08-10 10:23:15 -0300

    estrutura visual - tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,60 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import * as C from './styles'
+
 export function Transactions(){
   return (
     <div>
       <Header />
       <Summary />
+
+      <C.TransactionsContainer>
+        <C.TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <C.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </C.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Humburguer</td>
+              <td>
+                <C.PriceHighlight variant="outcome">
+                  -R$ 20,00
+                </C.PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <C.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </C.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <C.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </C.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </C.TransactionsTable>
+      </C.TransactionsContainer>
     </div>
   )
 }
