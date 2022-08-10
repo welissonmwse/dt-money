5d6555c311ec3fcc106ad539b06b262df461dd93
Welisson Silveira <welissonmwse@gmail.com>
2022-08-10 09:59:42 -0300

    estrutura visual - componente: Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,30 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import * as C from './styles'
+
+export function Summary(){
+  return (
+    <C.SummaryContainer>
+      <C.SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e"/>
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </C.SummaryCard>
+      <C.SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68"/>
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </C.SummaryCard>
+      <C.SummaryCard variant="green-700">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff"/>
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </C.SummaryCard>
+    </C.SummaryContainer>
+  )
+}
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const SummaryContainer = styled.section`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+
+  margin-top: -5rem;
+`
+
+interface SummaryCardProps {
+  variant?: 'green-700'
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background-color: ${(props) => props.variant ? props.theme[props.variant] : props.theme['gray-600']};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    color: ${(props) => props.theme['gray-300']};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+`
