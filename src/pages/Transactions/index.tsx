d91e408e72110d95447662f01f34c00c95cce2cc
Welisson Silveira <welissonmwse@gmail.com>
2022-08-11 10:38:53 -0300

    Conexão com a API - Requisição HTTP: Listando transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 
 import * as C from './styles'
 
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
 export function Transactions(){
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('http://localhost:3333/transactions')
+      const data = await response.json()
+      setTransactions(data)
+    })()
+  })
   return (
     <div>
       <Header />
@@ -13,46 +32,18 @@ export function Transactions(){
         <SearchForm />
         <C.TransactionsTable>
           <tbody>
-            <tr>
-              <td>Desenvolvimento de site</td>
-              <td>
-                <C.PriceHighlight variant="income">
-                  R$ 12.000,00
-                </C.PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td>Humburguer</td>
-              <td>
-                <C.PriceHighlight variant="outcome">
-                  -R$ 20,00
-                </C.PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td>Desenvolvimento de site</td>
-              <td>
-                <C.PriceHighlight variant="income">
-                  R$ 12.000,00
-                </C.PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td>Desenvolvimento de site</td>
-              <td>
-                <C.PriceHighlight variant="income">
-                  R$ 12.000,00
-                </C.PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td>{transaction.description}</td>
+                <td>
+                  <C.PriceHighlight variant={transaction.type}>
+                    {transaction.price}
+                  </C.PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
           </tbody>
         </C.TransactionsTable>
       </C.TransactionsContainer>
